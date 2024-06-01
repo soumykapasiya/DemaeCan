055e76fb162de4eff9b41aa137a12476f71a81fd
soyamkapasiya <163526543+soyamkapasiya@users.noreply.github.com>
2024-06-01 09:53:55 +0530

    fourth

---
+++ src/main/java/com/kapasiya/demaecan/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.kapasiya.demaecan.config;
+
+
+public class JwtConstant
+{
+    public static final String SECRET = "siberianstark#0012";
+    public static final String JWT_HEADER = "Authorization";
+
+}
